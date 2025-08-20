023a36d985194ad796424fb5db592de1f91b8b7b
akhiljacob1 <akhiljacob98@gmail.com>
2025-08-20 13:08:41 +0100

    Add ingredients

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_08_20_113747) do
+ActiveRecord::Schema[8.0].define(version: 2025_08_20_120442) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -24,5 +24,6 @@ ActiveRecord::Schema[8.0].define(version: 2025_08_20_113747) do
     t.string "image_url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "ingredients", default: [], array: true
   end
 end
