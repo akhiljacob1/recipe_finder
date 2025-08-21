eb68ef3ffe09ab27bb2013d575a9836709cc6e43
akhiljacob1 <akhiljacob98@gmail.com>
2025-08-21 12:08:34 +0100

    Add tests

--- test/controllers/recipes_controller_test.rb
+++ test/controllers/recipes_controller_test.rb
@@ -2,12 +2,27 @@ require "test_helper"
 
 class RecipesControllerTest < ActionDispatch::IntegrationTest
   setup do
-    @recipe = recipes(:one)
+    @recipe = recipes(:pancakes)
   end
 
   test "should get index" do
     get recipes_url
     assert_response :success
+  end
+
+  test "should get index with search parameters" do
+    get recipes_url, params: { ingredients: "eggs, flour" }
+    assert_response :success
+  end
+
+  test "index view should display search form" do
+    get recipes_url
+    assert_response :success
+    
+    # Check for search form elements
+    assert_select "form[action=?]", recipes_path
+    assert_select "input[name='ingredients']"
+    assert_select "input[type='submit']"
   end
 
   test "should get new" do
--- test/system/recipes_test.rb
+++ test/system/recipes_test.rb
@@ -2,12 +2,93 @@ require "application_system_test_case"
 
 class RecipesTest < ApplicationSystemTestCase
   setup do
-    @recipe = recipes(:one)
+    @recipe = recipes(:pancakes)
   end
 
   test "visiting the index" do
     visit recipes_url
     assert_selector "h1", text: "Recipes"
+  end
+
+  test "searching for recipes by ingredients" do
+    visit recipes_url
+    
+    # Should see the search form
+    assert_selector "form"
+    assert_selector "input[name='ingredients']"
+    assert_selector "input[type='submit']"
+    
+    # Search for eggs and flour
+    fill_in "ingredients", with: "eggs, flour"
+    click_button "Find Recipes"
+    
+    # Should see search results
+    assert_text "Showing recipes that match: eggs, flour"
+    assert_text "Found 3 matching recipes"
+    
+    # Should see the matching recipes (100% matches appear first)
+    assert_text "Fluffy Pancakes"
+    assert_text "Chocolate Chip Cookies"
+    # Carbonara appears too (50% match - has eggs but not flour)
+    assert_text "Classic Pasta Carbonara"
+    
+    # Should not see non-matching recipes
+    assert_no_text "Quick Chicken Stir Fry"
+  end
+
+  test "searching with single ingredient" do
+    visit recipes_url
+    
+    fill_in "ingredients", with: "garlic"
+    click_button "Find Recipes"
+    
+    # Should see search results with garlic-containing recipes
+    assert_text "Showing recipes that match: garlic"
+    assert_text "Creamy Tomato Soup"  # has garlic
+    assert_text "Quick Chicken Stir Fry"  # has garlic
+  end
+
+  test "searching with no matches" do
+    visit recipes_url
+    
+    fill_in "ingredients", with: "quinoa"
+    click_button "Find Recipes"
+    
+    # Should see search query but no results
+    assert_text "Showing recipes that match: quinoa"
+    assert_text "Found 0 matching recipes"
+    
+    # Should not see any recipe titles
+    assert_no_text "Fluffy Pancakes"
+    assert_no_text "Chocolate Chip Cookies"
+  end
+
+  test "clearing search shows all recipes" do
+    visit recipes_url
+    
+    # First do a search
+    fill_in "ingredients", with: "eggs"
+    click_button "Find Recipes"
+    assert_text "Showing recipes that match: eggs"
+    
+    # Click "Show All Recipes" link
+    click_link "Show All Recipes"
+    
+    # Should see all recipes again
+    assert_no_text "Showing recipes that match"
+    assert_text "Fluffy Pancakes"
+    assert_text "Quick Chicken Stir Fry"
+    assert_text "Creamy Tomato Soup"
+  end
+
+  test "search form preserves search query" do
+    visit recipes_url
+    
+    fill_in "ingredients", with: "tomatoes, cheese"
+    click_button "Find Recipes"
+    
+    # The search field should still contain the search query
+    assert_field "ingredients", with: "tomatoes, cheese"
   end
 
   test "should create recipe" do
